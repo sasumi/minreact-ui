61cea02158f47362d67bf0a2b545fbdd6174f3ec
swing <cnsasumi@gmail.com>
2026-09-02 20:33:53 +0800

    upd

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,20 +1,20 @@
-import { guid } from 'minutool';
-import { forwardRef, useState } from 'react';
+import { guid } from "minutool";
+import { forwardRef, useState } from "react";
 import "./../styles/components/image.scss";
 import { namespace } from "./../styles/namespace";
 
 const CSS_NS = namespace + "-img";
 
-const STATE_LOADING = 'loading';
-const STATE_ERROR = 'error';
-const STATE_EMPTY = 'empty';
-const STATE_NORMAL = 'normal';
+const STATE_LOADING = "loading";
+const STATE_ERROR = "error";
+const STATE_EMPTY = "empty";
+const STATE_NORMAL = "normal";
 
 const STATE_TITLE_MAP = {
-    [STATE_LOADING]: '加载中...',
-    [STATE_ERROR]: '图片加载失败',
-    [STATE_EMPTY]: '暂无图片',
-    [STATE_NORMAL]: '',
+    [STATE_LOADING]: "加载中...",
+    [STATE_ERROR]: "图片加载失败",
+    [STATE_EMPTY]: "暂无图片",
+    [STATE_NORMAL]: "",
 } as Record<string, string>;
 
 /**
@@ -23,13 +23,12 @@ const STATE_TITLE_MAP = {
  * @returns React 元素
  */
 export const ImageLoader = forwardRef<HTMLImageElement, React.ImgHTMLAttributes<HTMLImageElement>>(({ ...rest }, ref) => {
-    rest.loading = rest.loading ?? 'lazy';
     const [state, setState] = useState(!rest.src ? STATE_EMPTY : STATE_LOADING);
     return (
         <>
             <img
                 {...rest}
-                className={CSS_NS + ' ' + (rest.className || '')}
+                className={CSS_NS + " " + (rest.className || "")}
                 ref={ref}
                 data-state={state}
                 onLoad={(e) => {
@@ -41,7 +40,7 @@ export const ImageLoader = forwardRef<HTMLImageElement, React.ImgHTMLAttributes<
                     rest.onError?.(e);
                 }}
             />
-            <span className={CSS_NS + '__holder'} data-state={state} title={STATE_TITLE_MAP[state]} />
+            <span className={CSS_NS + "__holder"} data-state={state} title={STATE_TITLE_MAP[state]} />
         </>
     );
 });
@@ -52,19 +51,23 @@ export const ImageLoader = forwardRef<HTMLImageElement, React.ImgHTMLAttributes<
  * @returns HTML 字符串
  */
 export const patchImgLoader = ({ ...props }) => {
-    const img_id = guid('img');
-    const holder_id = guid('img__holder');
-    if(!props.src){
+    const img_id = guid("img");
+    const holder_id = guid("img__holder");
+    const isEmpty = !props.src;
+    if (isEmpty) {
         delete props.src;
     }
     props.class = props.class ? `${CSS_NS} ${props.class}` : CSS_NS;
     return `
         <img id="${img_id}" ${Object.entries(props)
             .map(([key, value]) => `${key}="${value}"`)
-            .join(' ')}
-            data-state="${!props.src ? STATE_EMPTY : STATE_LOADING}"
-            onload="this.dataset.state='normal';var h=document.getElementById('${holder_id}');if(h){h.dataset.state='normal'}"
-            onerror="this.dataset.state='error';var h=document.getElementById('${holder_id}');if(h){h.dataset.state='error'}"/>
-        <span id="${holder_id}" class="${CSS_NS}__holder" data-state="${STATE_LOADING}" title="${STATE_TITLE_MAP[STATE_LOADING]}"></span>
+            .join(" ")}
+            data-state="${isEmpty ? STATE_EMPTY : STATE_LOADING}"
+            ${
+                !isEmpty &&
+                `onload="this.dataset.state='normal';var h=document.getElementById('${holder_id}');if(h){h.dataset.state='normal'}"
+                onerror="this.dataset.state='error';var h=document.getElementById('${holder_id}');if(h){h.dataset.state='error'}"/>`
+            }
+        <span id="${holder_id}" class="${CSS_NS}__holder" data-state="${isEmpty ? STATE_EMPTY : STATE_LOADING}" title="${STATE_TITLE_MAP[STATE_LOADING]}"></span>
     `;
 };
